a6477f91c10c8c84843b7cf59ae369fb3d08c3cb
Duy Phạm <duypham@Duys-MacBook-Pro.local>
2019-04-19 18:58:33 +0700

    v 1.0.7

--- routes/api/graphs.js
+++ routes/api/graphs.js
@@ -5,14 +5,16 @@ const Graph = require('../../models/Graph.js');
 
 
 router.get('/', (req, res) => {
+	Graph.findById('5cb96927d745f9137ef93897')
+		.then((graph) => res.status(200).json(graph));
+	
 	const newGraph = new Graph({
 		ipAddress: req.connection.remoteAddress,
 	});
 	newGraph.save()
 	.then((graph) => res.json(graph));
 
-	Graph.findById('5cb96927d745f9137ef93897')
-		.then((graph) => res.status(200).json(graph));
+	
 })
 
 module.exports = router;
